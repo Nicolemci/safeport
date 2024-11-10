ea8266da1ba2e44b8adce6fc11bb930d2f1bbe92
Nicole Mcinerney <nicolemci@hotmail.com>
2024-11-10 10:24:38 +0000

    added google maps api

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,11 @@
 Airport.create(
   name: "London Heathrow",
   city: "London",
-  recommended: "Heathrow Express for fast, direct travel to central London",
-  fastest: "Heathrow Express - 15 minutes to Paddington",
-  cheapest: "London Underground (Piccadilly Line)",
-  public_transport: "Heathrow is connected to central London via the Piccadilly Line and several bus routes...",
-  express_train: "The Heathrow Express is the fastest option, taking 15 minutes to reach Paddington...",
+  recommended: "Take the London Underground Elizabeth Line for combination of speed and value. Direct from Terminal 2,3,4 and 5 to Farringdon in as little as 35 minutes and £13.30",
+  fastest: "Use the Heathrow Express for 15 minutes to Paddington, £25.00",
+  cheapest: "London Underground Picadilly line will get you to Picadilly Circus in 49 minutes for £5.60",
+  public_transport: "Heathrow is conntected to the London Underground on the Picadilly Line and Elizabeth lines and by Bus services.",
+  express_train: "The Heathrow Express is the fastest option to get to London, taking 15 minutes to reach Paddington Station.",
   taxi: "Taxis are available outside each terminal and cost around £50 to £70 to central London...",
   ride_apps: "Ride-hailing apps like Uber operate at Heathrow with designated pickup points...",
   tips: "Consider using the Heathrow Express during peak hours to avoid traffic delays."
